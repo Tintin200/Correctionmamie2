544f91366f7bd4304a06ac37c78b5a284edcd607
Ayman Mougou <aymoug@outlook.fr>
2024-02-23 09:54:24 +0100

    modif2

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\TypeCafe;
+use App\Form\ModifTypeCafeType;
+use App\Repository\TypeCafeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Repository\TypeCafeRepository;
 
 class BaseController extends AbstractController
 {
@@ -13,7 +17,7 @@ class BaseController extends AbstractController
     public function index(): Response
     {
         return $this->render('base/index.html.twig', [
-            
+
         ]);
     }
 
@@ -23,6 +27,22 @@ class BaseController extends AbstractController
         $typeCafes = $typeCafeRepository->findAll();
         return $this->render('base/liste-type-cafe.html.twig', [
             'typeCafes' => $typeCafes,
+        ]);
+    }
+
+    #[Route('/modif-type-cafe/{id}', name: 'app_modif_type_cafe')]
+    public function modifTypeCafe(TypeCafe $typeCafe, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ModifTypeCafeType::class, $typeCafe);
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($typeCafe);
+                $em->flush();
+            }
+        }
+        return $this->render('base/modif-type-cafe.html.twig', [
+            "form" => $form->createView(),
         ]);
     }
 }
